529dcd29fe34ca310ac3241d3844430a9da28619
Drpapadopoulou <82087214+dpap96@users.noreply.github.com>
2023-07-12 00:08:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 "use strict";
 
 //ο πιο σωστος τροπος ειναι ο καμβας να τρεξει οταν η σελιδα φορτωσει αρα θα την βαλω μεσα σε event listener:
-
-window.addEventListener("load",()=>{ //ο κωδικας θα εκτελεστει...αφου γινει load το window
+window.addEventListener("load",()=>{ 
     
-    const canv= document.querySelector("#canvas"); //παιρνω τον καμβα
+    const canv= document.querySelector("#canvas"); //selecting canva
     const cntx= canv.getContext("2d"); //οριζω διαστασεις στον καμβα
-    const submitButton = document.querySelector("input[type='submit']"); // Επιλέγουμε το κουμπί υποβολής
-    const colorInput = document.querySelector("input[type='color']"); // Επιλέγουμε το input του χρώματος
-    const clearButton = document.querySelector("#clear");
-    let selectedColor;
+    const submitButton = document.querySelector("#submitBtn"); // selecting Submit button
+    const colorInput = document.querySelector("#colorPicker"); // Επιλέγουμε το input του χρώματος
+    const clearButton = document.querySelector("#clearBtn");
+    const saveButton = document.querySelector("#saveBtn");
+    let selectedColor,link;
     let drawing=false; //declares if someone draws
 
-//set width & height
-canv.height=600; 
-canv.width=window.innerWidth //ο καμβας παιρνει ολο το πλατος του παραθυρου
+//set width & height of canva
+    canv.height=600; 
+    canv.width=window.innerWidth //ο καμβας παιρνει ολο το πλατος του παραθυρου
 
+//Listener for cleaning canva
 clearButton.addEventListener("click", () => {
   cntx.clearRect(0, 0, canv.width, canv.height);
   selectedColor = colorInput.value; // Ενημέρωση του επιλεγμένου χρώματος
 });
 
+//Listener for selecting the drawing color
 submitButton.addEventListener("click", (event) => {
-    event.preventDefault(); // Αποτρέπουμε την προεπιλεγμένη συμπεριφορά υποβολής της φόρμας
-    selectedColor = colorInput.value; // Παίρνουμε την τιμή του input του χρώματος
-    console.log("Επιλεγμένο χρώμα:", selectedColor); // Εκτυπώνουμε το επιλεγμένο χρώμα
-  });
+  event.preventDefault();
+  selectedColor = colorInput.value;
+  console.log("Επιλεγμένο χρώμα:", selectedColor);
+});
+
+//Listener for downloading the img
+saveButton.addEventListener("click", () => { //savebutton is clicked..
+  link = document.createElement('a'); 
+  link.href = canv.toDataURL('image/png');
+  link.download = 'drawing.png'; //the name of drawing which user will download
+  link.click(); //downloading the png img
+});
   
-//fat arrow function
+//fat arrow function for drawing
 let draw=(e)=>{
     if (!drawing) {
         return;
@@ -38,20 +48,20 @@ let draw=(e)=>{
     
         cntx.lineTo(mouseX, mouseY); // Δημιουργούμε γραμμή στις συντεταγμένες
         cntx.strokeStyle = selectedColor;
-        cntx.lineWidth = 10;
+        cntx.lineWidth = 5;
         cntx.stroke(); // Εμφανίζουμε τη γραμμή
       }
 }
-//pressed mouse
+//Listener for pressed mouse
 canv.addEventListener("mousedown",()=>{
         drawing = true;
 })
-//mouse is not pressed
+//Listener for no-pressed mouse
 canv.addEventListener("mouseup",()=>{
     cntx.beginPath(); //
     drawing=false; //δεν ζωγραφιζω
 })
-//drawing:
+//Listener for drawing
 canv.addEventListener("mousemove",draw)
 
 })
